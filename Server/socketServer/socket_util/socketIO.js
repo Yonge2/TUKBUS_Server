ed201944ba7dd40c05ce298e219d2c952d5b964b
Yonge2 <dlwodyd25@gmail.com>
2022-12-01 18:18:24 +0900

    socketServer

---
+++ Server/socketServer/socket_util/socketIO.js
@@ -0,0 +1,64 @@
+const connection = require('../db/test_db');
+const {saveMessage} = require('./chat_util');
+const {redisGetScard, redisGetSmembers, redisSrem} = require('../../util/redisUtil');
+
+
+const chatting = (io) =>{
+
+    const chat = io.of('/chat').on('connection', async(socket)=>{
+
+        console.log('connection');
+        socket.join(socket.roomID);
+        console.log(socket.roomID," / ", socket.userID);
+
+        //룸안에 있는 인원 수
+        let userCount = await redisGetScard(`${socket.roomID}_IN`);
+        console.log("InUser : ", userCount);
+
+        //룸안에 있는 인원
+        let roomIn = null;
+
+        //return userID
+        chat.to(socket.roomID).emit("in", socket.userID);
+        
+        //data{roomID, message, sender, sendTime}
+        socket.on('chat message', async(data)=>{
+            //임시방편
+            roomIn = await redisGetSmembers(`${socket.roomID}_IN`);
+            await console.log("여기", roomIn);
+            data.roomID = socket.roomID;
+            data.userID = socket.userID;
+            data.receiver = roomIn.filter((ele)=> ele !== socket.userID);
+            console.log(data);
+            //메세지 저장
+            await saveMessage(data);
+
+            chat.to(socket.roomID).emit('chat message', {
+                userID : data.userID,
+                msg : data.msg,
+                time : data.sendTime
+            });
+        })
+    
+        socket.on('disconnect', async()=>{
+            console.log('dis');
+
+            const sremResult = await redisSrem(`${socket.roomID}_IN`, `${socket.userID}`);
+            userCount = await redisGetScard(`${socket.roomID}_IN`);
+            roomIn = await redisGetSmembers(`${socket.roomID}_IN`);
+            console.log("삭제", sremResult, "유저수 : ", userCount);
+
+            if(!userCount) {
+                let sqlset = [false, socket.roomID];
+                let sql = "UPDATE chatInfo SET isLive = ? WHERE roomID = ?;";
+                connection.query(sql, sqlset, (err)=>{
+                    if(err) console.log("Edit isLive err : ", err);
+                })
+            }
+        })
+    })
+}
+ 
+
+
+module.exports = chatting;
---
+++ Server/socketServer/socket_util/socketUtil.js
@@ -0,0 +1,64 @@
+const connection = require('../../db/conMysql');
+const redisClient = require('../../db/redis');
+const {verify} = require('../../api/user/user_util/jwt_util');
+const dayjs = require('dayjs');
+
+const receiverCheck = (receiver) =>{
+    switch(receiver.length){
+        case 2:
+            return JSON.stringify({receiver1: receiver[0],
+                receiver2: receiver[1]});
+        case 1:
+            return JSON.stringify({receiver1: receiver[0]})
+        case 0:
+            return null;    
+    }
+}
+
+const saveMessage = async(obj) =>{
+    const date = new dayjs();
+    const message = {
+        roomID: obj.roomID,
+        sender: obj.userID,
+        message: obj.msg,
+        receiver: receiverCheck(obj.receiver),
+        sendTime: obj.sendTime,
+        sendDate: date.format('YYYY-MM-DD'),
+    }
+    console.log('insert message:', message);
+    let sql = "INSERT INTO chatmessage set ?;";
+    connection.query(sql, message, (err)=>{
+        if(err) console.log("ChatMessage DB err : ", err);
+    });
+    return message;
+}
+
+const socketJWTMiddleware = async(socket, next) => {
+    if (socket.handshake.auth.token && socket.handshake.auth.roomID) {
+        const token = socket.handshake.auth.token;
+        const result = verify(token);
+        if(result.success){
+            const userID = socket.userID = result.userID;
+            socket.roomID = socket.handshake.auth.roomID;
+            console.log(userID);
+
+            await redisClient.sAdd(`${socket.handshake.auth.roomID}_IN`, `${userID}`, (err, data)=>{
+                if(!err) {
+                    console.log("sAdd 결과 : ",data);
+                    next();
+                }
+                else console.log("socket middleware err: ", err);      
+            });
+        }
+        else console.log(result, token);
+    }
+    else {
+        //에러 추가해야함.
+        console.log("no exist header!");
+        next();
+    }
+}
+
+
+
+module.exports = {saveMessage, socketJWTMiddleware};
