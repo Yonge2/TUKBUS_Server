ce4c036d3249339a2894aa8d88a86a488d34f3b4
Yonge2 <dlwodyd25@gmail.com>
2022-12-01 18:14:07 +0900

    function created random number

---
+++ Server/util/utilFunc.js
@@ -0,0 +1,20 @@
+const makeRandomNum = (digits) =>{
+    let multip = 0;
+    switch(digits){
+        case 2: 
+            multip = 100;
+            break;
+        case 3:
+            multip = 1000;
+            break;
+        case 4:
+            multip = 10000;
+            break;
+    }
+    let ranNum = Math.floor((Math.random()*multip)).toString();
+    
+    if(ranNum.length <digits) return "0"+ranNum;
+    else return ranNum;
+}
+
+module.exports = {makeRandomNum};
