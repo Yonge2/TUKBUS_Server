12e67c65bcdebd1e9e6e6244a8697689da261c33
Yonge2 <dlwodyd25@gmail.com>
2023-03-05 22:30:51 +0900

    edit portNum

--- Server/app.js
+++ Server/app.js
@@ -1,20 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const bodyParser = require('body-parser');
+const port = require('./private/privatekey_Tuk').PORT;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-app.use('/api/getSchedule', require('./api/getSchedule'));
+app.use('/api/getSchedule', require('./api/scheduleApi/scheduleRouter'));
 app.use('/api/manage', require('./api/editSchedule'));
-app.use('/api/user', require('./api/user/userRouter'));
+app.use('/api/user', require('./api/userApi/userRouter'));
 app.use('/api/chatting', require('./api/chatApi/chatRouter'));
 
 
-const port = process.env.PORT || 3000;
 const server = app.listen(port, function(){
     console.log("Express server has started")
 })
