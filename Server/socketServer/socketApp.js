3a9b31d4392fcedaf35d7b5f2f497edad6484726
Yonge2 <dlwodyd25@gmail.com>
2023-04-15 17:25:59 +0900

    e

--- Server/socketServer/socketApp.js
+++ Server/socketServer/socketApp.js
@@ -3,6 +3,10 @@ const app = express();
 const port = require('../private/privatekey_Tuk').PORT.socketPort;
 const router = express.Router();
 
+const server = app.listen(port.socketPort, function(){
+    console.log("Socket server has started")
+})
+
 const chat = require('./socket_util/socketIO');
 const {socketJWTMiddleware} = require('./socket_util/socketUtil');
 const io = require('socket.io')(server);
@@ -12,9 +16,7 @@ io.of('/chat').use(socketJWTMiddleware);
 
 chat(io);
 
-const server = app.listen(port.socketPort, function(){
-    console.log("Socket server has started")
-})
+
 
 
 
