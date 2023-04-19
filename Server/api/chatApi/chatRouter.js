6443d76180945a84ef207d3f86800c0751e03415
Yonge2 <dlwodyd25@gmail.com>
2023-04-19 15:32:27 +0900

    m

--- Server/api/chatApi/chatRouter.js
+++ Server/api/chatApi/chatRouter.js
@@ -4,7 +4,7 @@ const {getChatlist, createChatRoom, ingChat} = require('./chatInfoUtil');
 const {outChatroom, saveMessage} = require('./chatUtil');
 const jwt_middleWare = require('../userApi/user_util/authMiddleware');
 
-router.get('/getchatlist', jwt_middleWare, ingChat);
+router.get('/getchatlist', jwt_middleWare, getChatlist);
 
 router.post('/createchatroom', jwt_middleWare, createChatRoom);
 
