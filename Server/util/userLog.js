83ed88c784d4e1608491ea2acdfcc91690d3c921
Yonge2 <dlwodyd25@naver.com>
2023-05-06 17:00:56 +0900

    add daily userlog

--- Server/util/userLog.js
+++ Server/util/userLog.js
@@ -1,16 +1,38 @@
 const requestIp = require('request-ip');
 const dayjs = require('dayjs');
+const scheduler = require('node-schedule');
+
 const redisClient = require('../db/redis');
-const { redisQuery } = require('../private/query');
+const {redisGetScard} = require('./redisUtil');
+const { redisQuery, logQuery } = require('../private/query');
+const {setMySQL} = require('../db/conMysql');
+
+let now = new dayjs().format('YYYYMMDD');
+let dailyReq = 0;
+
+ //service start
+ scheduler.scheduleJob('00 01 00 * * *', async()=>{
+    console.log('daily log 초기화');
+
+    let dailyUser = await redisGetScard(redisQuery.userLog(now));
+    await setMySQL(logQuery.dailyUser, [now, dailyUser, dailyReq]);
+    await redisClient.v4.del(redisQuery.userLog(now));
+
+    setTimeout(()=>{
+        now = new dayjs().format('YYYYMMDD');
+        dailyReq = 0;
+    },1000);
+ });
 
 const getLog = async(req)=> {
-    const now = new dayjs().format('YYYYMMDD');
     if(req.userID){
         await redisClient.sAdd(redisQuery.userLog(now), `${req.userID}`);
+        dailyReq += 1;
     }
     else {
         const ip = requestIp.getClientIp(req);
         await redisClient.sAdd(redisQuery.userLog(now), `${ip}`);
+        dailyReq += 1;
     }
 }
 
