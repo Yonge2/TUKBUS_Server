e1f5bf842643ca68a768c5a7720f582e132fd6d3
Yonge2 <dlwodyd25@naver.com>
2023-05-31 12:41:19 +0900

    add intropage

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 const port = require('./private/privatekey_Tuk').PORT.appPort;
 
-
+app.use(express.static(path.join(__dirname, 'introPage/build')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -14,6 +15,10 @@ app.use('/api/getSchedule', require('./api/scheduleApi/scheduleRouter'));
 //app.use('/api/manage', require('./api/editSchedule'));
 app.use('/api/user', require('./api/userApi/userRouter'));
 app.use('/api/chatting', require('./api/chatApi/chatRouter'));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/introPage/build/index.html')
+});
 
 
 const server = app.listen(port, '0.0.0.0', ()=> {
