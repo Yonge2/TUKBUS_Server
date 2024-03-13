c61a1cae0791826549ad9bf3524de042fb14b178
Yonge2 <dlwodyd25@naver.com>
2024-03-13 17:47:24 +0900

    fix: providers repositroy 오류 해결

--- Server/chat-server/src/chattings/chattings.module.ts
+++ Server/chat-server/src/chattings/chattings.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ChattingsService } from './chattings.service';
-import { ChattingsController } from './chattings.controller';
+import { Module } from '@nestjs/common'
+import { ChattingsService } from './chattings.service'
+import { ChattingsController } from './chattings.controller'
+import { ChattingsRepository } from './chattings.repository'
 
 @Module({
   controllers: [ChattingsController],
-  providers: [ChattingsService],
+  providers: [ChattingsService, ChattingsRepository],
 })
 export class ChattingsModule {}
